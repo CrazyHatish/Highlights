7a0464f226d3fb1a7657a5eb4b53e7d4e6b44eeb
Pedro Gomez <phg1608@gmail.com>
2018-03-28 23:37:45 +0200

    Download Gfycat clips from search

--- gfycat.py
+++ gfycat.py
@@ -5,13 +5,39 @@ import string
 import random
 import urllib.request as req
 
-url = "https://gfycat.com/SpiritedIllegalDrafthorse"
+with open("config.yaml") as file:
+    config = yaml.load(file.read())
 
-html = req.urlopen(url).read().decode('utf-8')
+rd_config = config["reddit"]
 
-exp = re.compile(r"https://giant.gfycat.com/\S*\.mp4")
-links = exp.findall(html)[0]
+reddit = praw.Reddit(client_id=rd_config["client_id"],
+                    client_secret=rd_config["client_secret"],
+                    password=rd_config["password"],
+                    user_agent=rd_config["user_agent"],
+                    username=rd_config["username"])
 
-with open("video.mp4", "wb") as file:
-    video = req.urlopen(links)
-    file.write(video.read())
+sub = reddit.subreddit(config["subreddit"])
+
+clips = []
+
+translator = str.maketrans(" ", "+")
+
+search_query = "flair:{} site:gfycat.com".format(config["flair"].translate(translator))
+
+for result in sub.search(search_query, time_filter=config["time_filter"], sort="top"):
+    clips.append(result.url)
+
+
+for i in range(config["clips"]):
+
+    print("Downloading Clips {}/{}".format(i+1, config["clips"]))
+
+    clip = req.urlopen(clips[i])
+
+    exp = re.compile(r"https://giant.gfycat.com/\S*\.mp4")
+    video_link = exp.findall(clip.read().decode("utf-8"))
+
+    video = req.urlopen(video_link[0])
+
+    with open("{}/video{}.mp4".format(config["workdir"], i), "wb") as file:
+        file.write(video.read())
